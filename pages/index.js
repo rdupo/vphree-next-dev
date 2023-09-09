d8769f5404607acae345d351758b276689ae24e6
rdupo <67809219+rdupo@users.noreply.github.com>
2023-09-09 16:21:07 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { React, useEffect } from 'react'
 import Link from 'next/link'
 import Header from  '../components/Header'
 import Footer from '../components/Footer'
