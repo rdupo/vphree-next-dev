4b74ed5818dd98e2132484f5ea4c64fb0d992113
rdupo <67809219+rdupo@users.noreply.github.com>
2024-01-20 18:52:35 -0600

    escaped quotes (prod prep)

--- components/Footer.js
+++ components/Footer.js
@@ -12,7 +12,7 @@ const Footer = ({ bg }) => {
 	return (
 		<div>
 			<div className={tcBg}>
-				<p>vPhree is provided on an "as is" and "as available" basis. The vPhree Team does not give any warranties and will not be liable for any loss, direct or indirect, through continued use of this app.</p>
+				<p>vPhree is provided on an &#34;as is&#34; and &#34;as available&#34; basis. The vPhree Team does not give any warranties and will not be liable for any loss, direct or indirect, through continued use of this app.</p>
 			</div>
 			<div className="footer black-bg drk-grey-txt flex">
 				<p>Be phree. Be phunky. Shout out to the <a href="https://notlarvalabs.com" target="_blank">NotLarvaLabs</a> team for paving the way ✊</p>
